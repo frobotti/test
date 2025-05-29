5782be7d2937f7f69e59d1f2f41a795b64f508a3
frobotti <frobotti@quantis.com.uy>
2025-05-29 12:57:05 -0300

    cors

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI
 from app.api import router as echo_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(echo_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Podés restringir esto más adelante
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
